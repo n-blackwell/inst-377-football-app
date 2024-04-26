86d882d4baf1d2dfe3b216bc853aa388e49f3522
n-black <nblackwell213@gmail.com>
2024-04-26 15:02:19 -0400

    set up express server and created a proxy in the app package to allow for server to interact with api in dev environment. added code to app to test this

---
+++ server/app.js
@@ -0,0 +1,27 @@
+const express = require("express")
+
+const app = express()
+
+const port = 9000
+
+
+var items = [
+    {
+        "name": "item1",
+        "price": "100"
+    },
+    {
+        "name": "item2",
+        "price": "200"
+    }
+]
+
+app.get('/api/items', (req, res) => {
+    res.send(items)
+
+})
+
+app.listen(port, () => {
+    console.log(`Server Listening on Port ${port}`)
+})
+
