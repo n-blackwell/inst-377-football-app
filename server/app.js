6ab60bf270e15ec2214c57a7fc9f7f8c5392ac5c
n-black <nblackwell213@gmail.com>
2024-05-13 12:31:28 -0400

    Added Supabase and Body Parser to Server

--- server/app.js
+++ server/app.js
@@ -1,27 +1,18 @@
 const express = require("express")
+const supabaseClient = require("@supabase/supabase-js")
+const bodyParser = require("body-parser")
+
 
 const app = express()
-
 const port = 9000
+app.use(bodyParser.json())
+app.use(express.static(__dirname + '/public'))
+
+const supabaseURL = 'https://qrsyjpzbwskvprkogtxa.supabase.co'
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFyc3lqcHpid3NrdnBya29ndHhhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTU2MTc3MTEsImV4cCI6MjAzMTE5MzcxMX0.AWplzUF32KSVE69nR13qcOiAiDedtelHlSdbW4nGLOg'
 
 
-var items = [
-    {
-        "name": "item1",
-        "price": "100"
-    },
-    {
-        "name": "item2",
-        "price": "200"
-    }
-]
-
-app.get('/api/items', (req, res) => {
-    res.send(items)
-
-})
-
-app.get('/api/team/dailyInfo', (req, res) => {
+app.get('/team/info', (req, res) => {
 
 })
 
