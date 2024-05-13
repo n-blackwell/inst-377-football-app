25bd28639cc2d942ab4051ef69ff13d007989b73
n-black <nblackwell213@gmail.com>
2024-05-13 15:01:49 -0400

    - Stats table on TeamsPage now pulls from Server - Server queries the database for team stats and if   the entry is over a day old the server calls the   external API and replaces the database entry

--- football-app/src/pages/Teams/TeamsPage.jsx
+++ football-app/src/pages/Teams/TeamsPage.jsx
@@ -25,11 +25,14 @@ function PlayerData({ playersRes }) {
 
 function TeamsPage() {
     const apiKey = '10755b2a08msh15dff173eafe850p158f43jsnf3d99db20514'
-    const url = "https://api-football-v1.p.rapidapi.com/v3/players/squads?team=33"
-    const [players, setPlayers] = useState([])
+    const playerUrl = "https://api-football-v1.p.rapidapi.com/v3/players/squads?team=33"
+    const statsUrl = "http://localhost:9000/team/info?teamID=33&LeagueID=39"
 
-    const fetchPlayers = async (url) => {
-        const res = await fetch(url, {
+    const [players, setPlayers] = useState([])
+    const [stats, setStats] = useState([])
+
+    const fetchPlayers = async (playerUrl) => {
+        const res = await fetch(playerUrl, {
             method: 'GET',
             headers: {
                 'X-RapidAPI-Key': '10755b2a08msh15dff173eafe850p158f43jsnf3d99db20514',
@@ -41,14 +44,23 @@ function TeamsPage() {
         setPlayers(data)
     }
 
+    const fetchTeamStats = async (statsUrl) => {
+        const res = await fetch(statsUrl)
+        const data = await res.json()
+        setStats(data[0])
+        console.log(data)
+    }
+
     useEffect(() => {
-        // fetchPlayers(url)
+        // fetchPlayers(playerUrl)
+        fetchTeamStats(statsUrl)
     }, [])
 
     return (
         <>
-            <h1>Teams Page</h1>
+            <h1 style={{ marginLeft: 10 }}>Teams Page</h1>
             <div className="roster-table">
+                <span><strong>Roster</strong></span>
                 <Table striped bordered>
                     <thead>
                         <tr>
@@ -58,12 +70,13 @@ function TeamsPage() {
                         </tr>
                     </thead>
                     <tbody>
-                        {/* <PlayerData playersRes={players} /> */}
+                        <PlayerData playersRes={players} />
                     </tbody>
                 </Table>
             </div>
             <div className="team-info-table">
-                <Table striped bordered>
+                <span><strong>Stats</strong></span>
+                <Table striped bordered >
                     <thead>
                         <tr>
                             <th>Category</th>
@@ -73,60 +86,59 @@ function TeamsPage() {
                         </tr>
                     </thead>
                     <tbody>
-                        {/* <TeamData teamRes={team} /> */}
                         <tr>
                             <td>Games Played</td>
-                            <td></td>
-                            <td></td>
-                            <td></td>
+                            <td>{stats['home_games']}</td>
+                            <td>{stats['away_games']}</td>
+                            <td>{stats['total_games']}</td>
                         </tr>
                         <tr>
                             <td>Wins</td>
-                            <td></td>
-                            <td></td>
-                            <td></td>
+                            <td>{stats['home_wins']}</td>
+                            <td>{stats['away_wins']}</td>
+                            <td>{stats['total_wins']}</td>
                         </tr>
                         <tr>
                             <td>Draws</td>
-                            <td></td>
-                            <td></td>
-                            <td></td>
+                            <td>{stats['home_draws']}</td>
+                            <td>{stats['away_draws']}</td>
+                            <td>{stats['total_draws']}</td>
                         </tr>
                         <tr>
                             <td>Losses</td>
-                            <td></td>
-                            <td></td>
-                            <td></td>
+                            <td>{stats['home_losses']}</td>
+                            <td>{stats['away_losses']}</td>
+                            <td>{stats['total_losses']}</td>
                         </tr>
                         <tr>
                             <td colSpan={4}><strong>Goals</strong></td>
                         </tr>
                         <tr>
                             <td>Goals For</td>
-                            <td></td>
-                            <td></td>
-                            <td></td>
+                            <td>{stats['for_home_goals']}</td>
+                            <td>{stats['for_away_goals']}</td>
+                            <td>{stats['for_total_goals']}</td>
                         </tr>
                         <tr>
                             <td>Goals Against</td>
-                            <td></td>
-                            <td></td>
-                            <td></td>
+                            <td>{stats['against_home_goals']}</td>
+                            <td>{stats['against_home_goals']}</td>
+                            <td>{stats['against_home_goals']}</td>
                         </tr>
                         <tr>
                             <td colSpan={4}><strong>Average Goals</strong></td>
                         </tr>
                         <tr>
                             <td>Goals For</td>
-                            <td></td>
-                            <td></td>
-                            <td></td>
+                            <td>{(stats['for_home_goals'] / stats['total_games']).toFixed(2)}</td>
+                            <td>{(stats['for_away_goals'] / stats['total_games']).toFixed(2)}</td>
+                            <td>{(stats['for_total_goals'] / stats['total_games']).toFixed(2)}</td>
                         </tr>
                         <tr>
                             <td>Goals Against</td>
-                            <td></td>
-                            <td></td>
-                            <td></td>
+                            <td>{(stats['against_away_goals'] / stats['total_games']).toFixed(2)}</td>
+                            <td>{(stats['against_away_goals'] / stats['total_games']).toFixed(2)}</td>
+                            <td>{(stats['against_total_goals'] / stats['total_games']).toFixed(2)}</td>
                         </tr>
                     </tbody>
                 </Table>
--- server/app.js
+++ server/app.js
@@ -11,9 +11,15 @@ app.use(express.static(__dirname + '/public'))
 const supabaseURL = 'https://qrsyjpzbwskvprkogtxa.supabase.co'
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFyc3lqcHpid3NrdnBya29ndHhhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTU2MTc3MTEsImV4cCI6MjAzMTE5MzcxMX0.AWplzUF32KSVE69nR13qcOiAiDedtelHlSdbW4nGLOg'
 
+const supabase = supabaseClient.createClient(supabaseURL, supabaseKey)
 
-app.get('/team/info', (req, res) => {
+app.get('/team/info', async (req, res) => {
+    const teamID = req.query.teamID
+    const leagueID = req.query.leagueID
 
+    const data = await getTeamInfo(teamID, leagueID)
+    console.log("Sending Team Info")
+    res.send(JSON.stringify(data))
 })
 
 
@@ -21,3 +27,80 @@ app.listen(port, () => {
     console.log(`Server Listening on Port ${port}`)
 })
 
+async function getTeamInfo(teamID, leagueID) {
+    const d = new Date();
+    let year = d.getFullYear() - 1;
+    console.log("Getting Team Info")
+    url = `https://api-football-v1.p.rapidapi.com/v3/teams/statistics?league=${leagueID}&season=${year}&team=${teamID}`
+    const { data, error } = await supabase
+        .from('teams')
+        .select()
+        .eq('team_id', teamID)
+    // console.log(data)
+    if (error) {
+        return {}
+    }
+
+    lastEntry = data[0]['created_at'].slice(0, 10)
+    unixLastEntry = Math.floor(new Date(lastEntry).getTime() / 1000)
+    unixCurrentTime = Math.floor(new Date().getTime() / 1000)
+
+    if (unixCurrentTime - unixLastEntry >= 84_000) {
+        console.log("Updating Database")
+        const res = await fetch(url, {
+            method: 'GET',
+            headers: {
+                'X-RapidAPI-Key': '10755b2a08msh15dff173eafe850p158f43jsnf3d99db20514',
+                'X-RapidAPI-Host': 'api-football-v1.p.rapidapi.com'
+            }
+        })
+        const resJson = await res.json()
+
+        // console.log("New Response Data", resJson)
+        const { upData, upError } = await supabase
+            .from('teams')
+            .update(cleanTeamJson(resJson))
+            .eq('id', data[0]['id'])
+            .select()
+        if (upError) {
+            return data
+        }
+        console.log("Returning Updated Data from Database")
+        // console.log(upData)
+        return upData
+    }
+    console.log("Returning Data from Database")
+
+    return data
+}
+
+function cleanTeamJson(obj) {
+    let newObj = {}
+    newObj["created_at"] = new Date()
+    newObj['home_games'] = obj['response']['fixtures']['played']['home']
+    newObj['away_games'] = obj['response']['fixtures']['played']['away']
+    newObj['total_games'] = obj['response']['fixtures']['played']['total']
+
+    newObj['home_wins'] = obj['response']['fixtures']['wins']['home']
+    newObj['away_wins'] = obj['response']['fixtures']['wins']['away']
+    newObj['total_wins'] = obj['response']['fixtures']['wins']['total']
+
+    newObj['home_draws'] = obj['response']['fixtures']['draws']['home']
+    newObj['away_draws'] = obj['response']['fixtures']['draws']['away']
+    newObj['total_draws'] = obj['response']['fixtures']['draws']['total']
+
+    newObj['home_losses'] = obj['response']['fixtures']['loses']['home']
+    newObj['away_losses'] = obj['response']['fixtures']['loses']['away']
+    newObj['total_losses'] = obj['response']['fixtures']['loses']['total']
+
+    newObj['for_home_goals'] = obj['response']['goals']['for']['total']['home']
+    newObj['for_away_goals'] = obj['response']['goals']['for']['total']['away']
+    newObj['for_total_goals'] = obj['response']['goals']['for']['total']['total']
+
+    newObj['against_home_goals'] = obj['response']['goals']['against']['total']['home']
+    newObj['against_away_goals'] = obj['response']['goals']['against']['total']['away']
+    newObj['against_total_goals'] = obj['response']['goals']['against']['total']['total']
+    console.log('JSON Cleaned')
+    // console.log(newObj)
+    return newObj
+}
