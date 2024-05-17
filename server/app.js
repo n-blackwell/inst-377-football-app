4793ddfc8f40cc949ae70f16289eb72b65ac0bc0
n-black <nblackwell213@gmail.com>
2024-05-17 14:00:58 -0400

    - Created first build file and added it to the   server files - Made server serve the front-end rather than   having them run seperately - BUG: Front-end is no longer loading back-end   data. Need to research

--- server/app.js
+++ server/app.js
@@ -1,12 +1,17 @@
 const express = require("express")
 const supabaseClient = require("@supabase/supabase-js")
 const bodyParser = require("body-parser")
+const path = require('path');
+
 
 
 const app = express()
 const port = 9000
 app.use(bodyParser.json())
-app.use(express.static(__dirname + '/public'))
+// Serves the front end using the server
+app.use(express.static(path.join(__dirname, './client/build')));
+// Runs the server and front-end on seperate local hosts
+// app.use(express.static(__dirname + '/public'))
 
 const supabaseURL = 'https://qrsyjpzbwskvprkogtxa.supabase.co'
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFyc3lqcHpid3NrdnBya29ndHhhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTU2MTc3MTEsImV4cCI6MjAzMTE5MzcxMX0.AWplzUF32KSVE69nR13qcOiAiDedtelHlSdbW4nGLOg'
@@ -18,6 +23,11 @@ const supabase = supabaseClient.createClient(supabaseURL, supabaseKey)
 app.listen(port, () => {
     console.log(`Server Listening on Port ${port}`)
 })
+
+// Lets react handle  the routing between pages
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname + '/client/build/index.html'));
+});
 
 // API Call for getting team info
 app.get('/team/info', async (req, res) => {
